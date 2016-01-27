ebbce7ee64270c31c216aaf07a942eb1d1a417fe
Martin Savoie <martin.savoie.2@ulaval.ca>
2016-01-27 18:50:14 -0500

    Adding TOTO for nodesJS performance

---
+++ TOTO/Node/chatserver.js
@@ -0,0 +1,26 @@
+var messages = [];
+
+exports.onConnection = function(socket){
+  socket.emit('getMessages', messages);
+  socket.on('newMessage', function(mess){
+    onNewMessage(socket);
+  });
+}
+
+function onNewMessage(socket){
+  var pi = estimatePi();
+  socket.broadcast.emit('getMessage', pi);
+}
+
+function estimatePi() {
+    var n = 10000000, inside = 0, i, x, y;
+
+    for ( i = 0; i < n; i++ ) {
+        x = Math.random();
+        y = Math.random();
+        if ( Math.sqrt(x * x + y * y) <= 1 )
+            inside++;
+    }
+
+    return 4 * inside / n;
+}
---
+++ TOTO/Node/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var chat = require('./chatserver');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', chat.onConnection);
+
+http.listen(args[0] || 3000);
+console.log('App started at port 3000!!!');
