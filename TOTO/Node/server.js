f368c508bdd79da6d753f8ee83d3e082233c3990
Maxime Charron <charron.maxime97@gmail.com>
2016-01-27 19:56:18 -0500

    Fix

--- TOTO/Node/server.js
+++ TOTO/Node/server.js
@@ -6,12 +6,7 @@ var io = require('socket.io')(http);
 
 var chat = require('./chatserver');
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', chat.onConnection);
 
-http.listen(args[0] || 3000);
+http.listen(3000);
 console.log('App started at port 3000!!!');
