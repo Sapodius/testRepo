4acd0db683b571dd6bf93b91252704bd6a774e49
Maxime Charron <charron.maxime97@gmail.com>
2016-01-28 07:49:33 -0500

    Do it in the heroku manner

--- TOTO/Node/server.js
+++ TOTO/Node/server.js
@@ -1,12 +1,22 @@
 require('newrelic');
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
+var WebSocketServer = require("ws").Server
 
 var chat = require('./chatserver');
 
-io.on('connection', chat.onConnection);
+//io.on('connection', chat.onConnection);
+//
+//http.listen(process.env.PORT || 3000);
+//console.log('App started at port 3000!!!');
 
-http.listen(process.env.PORT || 3000);
-console.log('App started at port 3000!!!');
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", chat.onConnection());
